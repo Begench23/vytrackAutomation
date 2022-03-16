fcacadd52c83f5c1dbd158f4c06f0aee88db72d0
Elminur Ablimit <elminur226@gmail.com>
2022-03-15 23:43:39 -0700

    user story_58 completed

--- src/test/java/com/vytrack/tests/vytrackLogInTest/UserStory_58.java
+++ src/test/java/com/vytrack/tests/vytrackLogInTest/UserStory_58.java
@@ -18,7 +18,6 @@ public class UserStory_58 extends TestBase {
         VytrackUtils.loginAsStoreManger();
         VytrackUtils.waitTillLoaderMaskDisappear();
 
-
         //verify Login successfully
         String actualTitle = Driver.getDriver().getTitle();
         String expectedTitle = "Dashboard";
@@ -47,7 +46,6 @@ public class UserStory_58 extends TestBase {
         List<String> expectedTitleName = new ArrayList<>(Arrays.asList("MODEL NAME", "MAKE", "CAN BE REQUESTED",
                 "CVVI", "CO2 FEE (/MONTH)", "COST (DEPRECIATED)", "TOTAL COST (DEPRECIATED)",
                 "CO2 EMISSIONS", "FUEL TYPE", "VENDORS"));
-
 
         // verify expected and actual
         Assert.assertEquals(expectedTitleName, ActualTitleNames);
@@ -85,10 +83,37 @@ public class UserStory_58 extends TestBase {
                 "CVVI", "CO2 FEE (/MONTH)", "COST (DEPRECIATED)", "TOTAL COST (DEPRECIATED)",
                 "CO2 EMISSIONS", "FUEL TYPE", "VENDORS"));
 
-
         Assert.assertEquals(expectedTitleName, actualTitleNames);
 
     }
 
+    @Test
+    public void truckDriverLogIn() {
+        VytrackUtils.loginAsDriver();
+        VytrackUtils.waitTillLoaderMaskDisappear();
+
+        //verify Login successfully
+        String actualTitle = Driver.getDriver().getTitle();
+        String expectedTitle = "Dashboard";
+        Assert.assertEquals(actualTitle, expectedTitle);
+
+        // locate "Fleet" tab
+        WebElement locateFleet = Driver.getDriver().findElement(By.xpath("//span[normalize-space()='Fleet'and contains(@class,'title title-level-1')]"));
+        Actions actions = new Actions(Driver.getDriver());
+        actions.moveToElement(locateFleet).perform();
+
+        // locate vehicle module button
+        WebElement vehicleModuleButton = Driver.getDriver().findElement(By.xpath("//a[@href='/entity/Extend_Entity_VehiclesModel']/span[@class='title title-level-2']"));
+        vehicleModuleButton.click();
+        VytrackUtils.waitTillLoaderMaskDisappear();
+
+        //locate error message
+        WebElement errorMessage = Driver.getDriver().findElement(By.xpath("//div[.='You do not have permission to perform this action.']"));
+        String actualMessage = errorMessage.getText();
+        String expectedMessage = "You do not have permission to perform this action.";
+
+        // verify error message
+        Assert.assertEquals(actualMessage, expectedMessage);
+    }
 }
 
